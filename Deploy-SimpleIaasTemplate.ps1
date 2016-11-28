52f9f7bbbab2a89eb616406b4cf5eddba9304923
adebisioje <bisioje@gmail.com>
2016-11-28 15:21:55 -0500

    Deploy IaaS ARM templates

---
+++ Deploy-SimpleIaasTemplate.ps1
@@ -0,0 +1,26 @@
+﻿### Define variables
+{
+$location = 'EAST US'
+$resourceGroupName = 'arm-simple-paas-template'
+$resourceDeploymentName = 'arm-iaas-template-deployment'
+$templatePath = 'C:\Users\adoje\Documents\GitHub\Azure-Resource-Manager-\'
+$templateFile = 'simpleIaasARMTemplate.json'
+$template = $templatePath + '\' + $templateFile
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $Location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
